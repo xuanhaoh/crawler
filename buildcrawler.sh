31845a82830c5b6a02283373b89ec3e02eeed879
xuanhaoh <xuanhaoh129@gmail.com>
2020-05-26 23:03:47 +1000

    fix

--- buildcrawler.sh
+++ buildcrawler.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker rmi -f crawler:2.0 xuanhaoh/crawler:2.0
-docker build -t crawler:2.0 .
-docker tag crawler:2.0 xuanhaoh/crawler:2.0
-docker push xuanhaoh/crawler:2.0
+docker rm -f crawler
+docker rmi -f crawler
+docker build -t crawler .
+docker run -d --name=crawler --env HOST=$(hostname -s) --env IP=$(hostname -I | cut -f 1 -d " ") crawler
